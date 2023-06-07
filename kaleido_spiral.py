bcafb6cd5898929bf82c56f8717eb3b1f7afb8d7
sivanesan-kishopika <135708964+sivanesan-kishopika@users.noreply.github.com>
2023-06-07 10:30:55 +0530

    Add files via upload

---
+++ kaleido_spiral.py
@@ -0,0 +1,17 @@
+import turtle
+
+turtle.bgcolor('black')
+turtle.speed('fast')
+turtle.pensize(4)
+turtle.pencolor('red')
+turtle.circle(30)
+
+def draw_circle(size):
+        turtle.pencolor('red')
+        turtle.circle(size)
+        draw_circle(size)
+
+turtle.bgcolor('black')
+turtle.speed('fast')
+turtle0pensize(4)
+draw_circle(30)
